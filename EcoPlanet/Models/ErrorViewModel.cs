387c631b0fa0d5c9a8f974637cd3ac2f48a9c02e
user <user@LAPTOP-FTV9ME34>
2024-03-25 16:32:33 +0800

    Add project files.

---
+++ EcoPlanet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoPlanet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
