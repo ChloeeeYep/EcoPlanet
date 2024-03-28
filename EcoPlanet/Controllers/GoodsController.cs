619a538130635ee38952c1dde0594208a1e60bb4
YEP XIN YUN <TP055957@mail.apu.edu.my>
2024-03-28 21:44:29 +0800

    S3_Store_GoodsImage

--- EcoPlanet/Controllers/GoodsController.cs
+++ EcoPlanet/Controllers/GoodsController.cs
@@ -4,14 +4,43 @@ using EcoPlanet.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using EcoPlanet.Areas.Identity.Data;
+using Amazon; //account purpose
+using Amazon.S3; //creating s3 process
+using Amazon.S3.Model; //object structure in S3
+using Microsoft.Extensions.Configuration; //accessing apps.settings
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal; //for object data transmissions in network
 
 
 namespace EcoPlanet.Controllers
 {
 	public class GoodsController : Controller
 	{
+		private const string bucketname = "ecoplanet";
+
 		private readonly EcoPlanetContext _context;
 		private readonly UserManager<EcoPlanetUser> _userManager;
+
+		//function 1:create a function to retrive the keys back from json file
+		private List<string> getKeys()
+		{
+			//1.1 create empty lists for storing keys
+			List<string> keylist = new List<string>();
+
+			//1.2 Get the keys back from json
+			var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+
+            IConfigurationRoot conf = builder.Build();
+
+			//1.3 Add the retrived keys to the list
+			keylist.Add(conf["Keys:Key1"]);
+			keylist.Add(conf["Keys:Key2"]);
+			keylist.Add(conf["Keys:Key3"]);
+
+            //1.4 return the key lists
+            return keylist;
+		}
 
 		public GoodsController(EcoPlanetContext context, UserManager<EcoPlanetUser> userManager)
 		{
@@ -21,19 +50,55 @@ namespace EcoPlanet.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			var user = await _userManager.GetUserAsync(User); // Gets the current logged-in user
-			if (user == null)
+            var user = await _userManager.GetUserAsync(User); // Gets the current logged-in user
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var email = user.Email;
+            var goods = await _context.GoodsTable
+                                      .Where(g => g.SellerId == email)
+                                      .ToListAsync();
+
+            //1.connect to the AWS account
+            List<string> keys = getKeys();
+            AmazonS3Client agent = new AmazonS3Client(keys[0], keys[1], keys[2], RegionEndpoint.USEast1);
+
+			//2. create empty lists that can store the retrieved images from S3
+			List<S3Object> imagelist = new List<S3Object>();
+
+			//3.read image by image and store to the lists
+			string ? nextToken = null;
+			do
 			{
-				return RedirectToAction("Login", "Account");
+				//3.1 Create Lists Request
+				ListObjectsRequest request = new ListObjectsRequest
+				{
+					BucketName = bucketname
+				};
+
+				//3.2 execute the request
+				ListObjectsResponse response = await agent.ListObjectsAsync(request);
+
+				//3.3 Store the images from response to the list
+				imagelist.AddRange(response.S3Objects);
+
+				//3.4 check the next addressing and store into the next token
+				nextToken = response.NextMarker;
+
 			}
+			while (nextToken != null);
 
-			var email = user.Email; 
-			var goods = await _context.GoodsTable
-									  .Where(g => g.SellerId == email)
-									  .ToListAsync();
+            // Create the ViewModel and populate it with goods and image data
+            var viewModel = new GoodsViewModel
+            {
+                GoodsList = goods,
+                ImageList = imagelist
+            };
 
-			return View(goods);
-		}
+            return View(viewModel);
+        }
 
 		public IActionResult AddGoods()
 		{
@@ -42,19 +107,66 @@ namespace EcoPlanet.Controllers
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> AddGoods(Goods goods)
-		{
-			if(ModelState.IsValid)
-			{
-				_context.GoodsTable.Add(goods);
-				await _context.SaveChangesAsync();
-				return RedirectToAction("Index");
-			}
-			return View(goods);			
+        public async Task<IActionResult> AddGoods(Goods goods, List<IFormFile> goodsImage)
+        {
+            // 1. Connect to the AWS account and retrieve keys
+            List<string> keys = getKeys();
+            AmazonS3Client agent = new AmazonS3Client(keys[0], keys[1], keys[2], RegionEndpoint.USEast1);
 
-		}
+            // 2. Upload the image file(s) to S3 and save the filename(s) to the goodsImage property
+            foreach (var image in goodsImage)
+            {
+                // Check for valid image file
+                if (image.Length <= 0)
+                {
+                    return BadRequest("Image of " + image.FileName + " is an empty file. Not able to upload.");
+                }
+                else if (image.Length > 2097152) // 2MB limit
+                {
+                    return BadRequest("Image of " + image.FileName + " is more than 2MB. Not able to upload.");
+                }
+                else if (image.ContentType.ToLower() != "image/png" && image.ContentType.ToLower() != "image/jpeg")
+                {
+                    return BadRequest("Image of " + image.FileName + " is an invalid file type. Not able to upload.");
+                }
 
-		public async Task<IActionResult> EditGoods(int ? goodsId)
+                // Upload image to S3
+                try
+                {
+                    PutObjectRequest request = new PutObjectRequest
+                    {
+                        InputStream = image.OpenReadStream(),
+                        BucketName = bucketname,
+                        Key = "GoodsImages/" + image.FileName, // Specify the S3 key where the image will be stored
+                        CannedACL = S3CannedACL.PublicRead // Set the ACL to allow public read access
+                    };
+
+                    await agent.PutObjectAsync(request);
+
+                    // Assign the filename (key) to the goodsImage property
+                    goods.goodsImage = "GoodsImages/" + image.FileName;
+                }
+                catch (AmazonS3Exception ex)
+                {
+                    return BadRequest("Image of " + image.FileName + " is facing technical issues in AWS Side. Error:  " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Image of " + image.FileName + " is facing technical issues. Error: " + ex.Message);
+                }
+            }
+
+            // Save the goods object to the database
+            if (ModelState.IsValid)
+            {
+                _context.GoodsTable.Add(goods);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(goods);
+        }
+
+        public async Task<IActionResult> EditGoods(int ? goodsId)
 		{
 			if(goodsId == null)
 			{
@@ -71,42 +183,91 @@ namespace EcoPlanet.Controllers
 
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> UpdateGoods(Goods goods)
-		{
-			try
-			{
-				if(ModelState.IsValid)
-				{
-					_context.GoodsTable.Update(goods);
-					await _context.SaveChangesAsync();
-					return RedirectToAction("Index","Goods");
-					
-				}
-				return View("EditGoods", goods);
-			}
-			catch (Exception ex)
-			{
-				return BadRequest("Error: " + ex.Message);
-			}
-		}
-
-		public async Task<IActionResult> DeleteGoods(int ? goodsId)
-		{
-            if (goodsId == null)
+        public async Task<IActionResult> UpdateGoods(Goods goods, List<IFormFile> goodsImage)
+        {
+            foreach (var image in goodsImage)
             {
-				return NotFound();
+                try
+                {
+                    List<string> keys = getKeys();
+                    AmazonS3Client agent = new AmazonS3Client(keys[0], keys[1], keys[2], RegionEndpoint.USEast1);
+
+                    //upload to S3
+                    PutObjectRequest request = new PutObjectRequest
+                    {
+                        InputStream = image.OpenReadStream(),
+                        BucketName = bucketname,
+                        Key = "GoodsImages/" + image.FileName, // Specify the S3 key where the image will be stored
+                        CannedACL = S3CannedACL.PublicRead // Set the ACL to allow public read access
+                    };
+
+                    await agent.PutObjectAsync(request);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error: " + ex.Message);
+                }
+
+                // Assign the filename (key) to the goodsImage property
+                goods.goodsImage = "GoodsImages/" + image.FileName;
             }
 
-			var goods = await _context.GoodsTable.FindAsync(goodsId);
-			if(goods == null)
-			{
-				return BadRequest(goodsId + " is not found from the lists");
-			}
-
-			_context.GoodsTable.Remove(goods);
-			await _context.SaveChangesAsync();
-			return RedirectToAction("Index", "Goods");
-
+            if (ModelState.IsValid)
+            {
+                _context.GoodsTable.Update(goods);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Goods");
+            }
+            else
+            {
+                return View("EditGoods", goods);
+            }
         }
-	}
+
+        public async Task<IActionResult> DeleteGoods(int ? goodsId)
+        {
+            try
+            {
+                if (goodsId == null)
+                {
+                    return NotFound();
+                }
+
+                var goods = await _context.GoodsTable.FindAsync(goodsId);
+
+                if (goods == null)
+                {
+                    return NotFound();
+                }
+
+                List<string> keys = getKeys(); // Retrieve AWS keys
+
+                AmazonS3Client agent = new AmazonS3Client(keys[0], keys[1], keys[2], RegionEndpoint.USEast1);
+
+                // Construct the S3 object key
+                string s3ObjectKey = goods.goodsImage;
+
+                // Check if the object exists in the S3 bucket before attempting deletion
+                DeleteObjectRequest deleteRequest = new DeleteObjectRequest
+                {
+                    BucketName = bucketname,
+                    Key = s3ObjectKey,
+                };
+
+
+                await agent.DeleteObjectAsync(deleteRequest);
+
+                // Remove the Trashpedia record from the database
+                _context.GoodsTable.Remove(goods);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Goods");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error deleting goods: " + ex.Message);
+            }
+        }
+    }
+	
 }
---
+++ EcoPlanet/Models/GoodsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Amazon.S3.Model;
+using System.Collections.Generic;
+
+namespace EcoPlanet.Models
+{
+    public class GoodsViewModel
+    {
+        public List<Goods> GoodsList { get; set; }
+        public List<S3Object> ImageList { get; set; }
+    }
+}
