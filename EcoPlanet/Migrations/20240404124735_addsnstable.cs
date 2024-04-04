e1459d2171701b6d2a95d88a44ac9a72c2f7cdeb
ZheeYoong <ZheeYoong@ZHEEYOONG-LAPTO>
2024-04-04 21:58:05 +0800

    displaynews

---
+++ EcoPlanet/Migrations/20240404124735_addsnstable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoPlanet.Migrations
+{
+    /// <inheritdoc />
+    public partial class addsnstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SNSTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SNSTable", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SNSTable");
+        }
+    }
+}
---
+++ EcoPlanet/Models/SNS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace EcoPlanet.Models
+{
+    public class SNS
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
